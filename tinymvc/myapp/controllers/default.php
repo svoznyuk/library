e11f4ceb4b39418e67d31be2a8fbe6b10a5c5292
Steve Voznyuk <s-svoznyu2@labmail.unl.edu>
2012-11-08 13:27:11 -0600

    login form

--- tinymvc/myapp/controllers/default.php
+++ tinymvc/myapp/controllers/default.php
@@ -11,10 +11,28 @@
 
 class Default_Controller extends TinyMVC_Controller
 {
-  function index()
-  {
-    $this->view->display('index_view');
-  }
+	function index($creds = array()) {
+
+		$this->view->display('login_view');
+		if(!empty($creds)) {
+			echo $creds['user'];
+			echo $creds['pass'];
+		}
+	}
+	  
+	function login() {
+		$pass = $_POST["u_pass"];
+		$uname = $_POST["u_name"];
+		if($pass === "pass" && $uname === "user") {
+			$this->main();
+		} else {
+			$this->index(array("user"=> $uname, "pass" => $pass));
+		}
+	}
+	
+	function main() {
+		$this->view->display('index_view');
+	}
 }
 
 ?>
---
+++ tinymvc/myapp/views/login_view.php
@@ -0,0 +1,11 @@
+<div class="wrapper">
+	<div id="login">
+		<form method="post" action="/index.php/login" id="login-form">
+			<label for="u-name">User Name:</label>
+			<input type="text" name="u_name" id="u-name" />
+			<lable for="u-password">Password:</lable>
+			<input type="password" name="u_pass" id="u-password" />
+			<input type="submit" name="sign_in" value="Sign In" />
+		</form>
+	</div>
+</div>
