e1bff78810312488cb310b064111211bfb5ed493
Steve Voznyuk <s-svoznyu2@labmail.unl.edu>
2012-11-13 18:32:20 -0600

    testing on cse

--- tinymvc/myapp/views/login_view.php
+++ tinymvc/myapp/views/login_view.php
@@ -1,11 +1,28 @@
-<div class="wrapper">
-	<div id="login">
-		<form method="post" action="/index.php/login" id="login-form">
-			<label for="u-name">User Name:</label>
-			<input type="text" name="u_name" id="u-name" />
-			<lable for="u-password">Password:</lable>
-			<input type="password" name="u_pass" id="u-password" />
-			<input type="submit" name="sign_in" value="Sign In" />
-		</form>
-	</div>
-</div>
+<html>
+	<head>
+		<title>Library Login</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+		<link rel="stylesheet" href="/css/site.css" type="text/css">
+	</head>
+	<body>
+		<div class="wrapper">
+			<h2>Wecome to the Library</h2>
+			<h4>Please sign in</h4>
+			<div id="login">
+				<form method="post" action="/index.php/login" id="login-form">
+					<div>
+						<label for="u-name">User Name:</label>
+						<input type="text" name="u_name" id="u-name" />
+					</div>
+					<div>
+						<label for="u-password">Password:</label>
+						<input type="password" name="u_pass" id="u-password" />
+					</div>
+					<div id='submit'>
+						<input type="submit" name="sign_in" value="Sign In" />
+					</div>
+				</form>
+			</div>
+		</div>
+	</body>
+</html>
